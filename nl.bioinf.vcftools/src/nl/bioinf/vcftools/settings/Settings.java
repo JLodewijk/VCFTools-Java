65fea1a7ae3a3d3648cd24feb3e95f09ded6474f
Sergio <Sergio@notebook>
2013-12-06 15:34:39 +0100

    initiated settings package

---
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/settings/Settings.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.bioinf.vcftools.settings;
+import java.io.IOException;
+
+/**
+ *
+ * @author Sergio Bondietti
+ */
+public class Settings {
+
+    private static Settings instance;
+    private SettingsBasic settingsBasic;
+    private SettingsSiteFilters settingsSiteFilters;
+    private SettingsIndividualFilters settingsIndividualFilters;
+
+    
+    /**
+     * Load settings files into memory
+     * @author Sergio Bondietti
+     * @throws IOException
+     */
+    public void load() throws IOException {
+
+    }
+
+    /**
+     * Save settings from memory to file
+     * @author Sergio Bondietti
+     * @throws IOException
+     */
+    public void save() throws IOException {
+
+    }
+
+    public static Settings getInstance() {
+        return instance;
+    }
+
+    public SettingsBasic getSettingsBasic() {
+        return settingsBasic;
+    }
+
+    public SettingsSiteFilters getSettingsSiteFilters() {
+        return settingsSiteFilters;
+    }
+
+    public SettingsIndividualFilters getSettingsIndividualFilters() {
+        return settingsIndividualFilters;
+    }
+
+}
