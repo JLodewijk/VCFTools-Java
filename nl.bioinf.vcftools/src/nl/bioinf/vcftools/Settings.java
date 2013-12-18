2b6353cdbf763fcf947118d90438faa7e5e93c35
sbondietti <sbondietti@bin257.bin>
2013-12-18 11:50:50 +0100

    Changed primitive types to class types

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/Settings.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/Settings.java
@@ -29,73 +29,73 @@ public class Settings {
     /* Basic Settings */
     private String inputFile;
     private String outputFile;
-    private boolean gzipped;
+    private Boolean gzipped;
 
     /* Site Filters */
     private ArrayList<String> chr;
     private ArrayList<String> notChr;
-    private int fromBp;
-    private int toBp;
+    private Integer fromBp;
+    private Integer toBp;
     private ArrayList<String> snp;
     private String snpFile;
     private ArrayList<String> excludeSnp;
     private String excludeSnpFile;
-    private ArrayList positions;
+    private ArrayList<Integer> positions;
     private String positionsFile;
     private ArrayList<Integer> excludePositions;
     private String excludePositionsFile;
-    private boolean keepOnlyIndels;
-    private boolean removeIndels;
+    private Boolean keepOnlyIndels;
+    private Boolean removeIndels;
     private String bedFile;
     //private Bed bed;
     //private Bed exludeBed;
-    private boolean removeFilteredAll;
+    private Boolean removeFilteredAll;
     private ArrayList<String> removeFiltered;
     private ArrayList<String> keepFiltered;
     private ArrayList<String> removeInfo;
     private ArrayList<String> keepInfo;
-    private double minQ;
-    private double minMeanDp;
-    private double maxMeanDp;
-    private double maf;
-    private double maxMaf;
-    private double nonRefAf;
-    private double maxNonRefAf;
-    private int mac;
-    private int maxMac;
-    private double nonRefAc;
-    private double maxNonRefAc;
-    private double hwe;
-    private double geno;
-    private int maxMissingCound;
-    private int minAlleles;
-    private int maxAlleles;
-    private int thin;
+    private Double minQ;
+    private Double minMeanDp;
+    private Double maxMeanDp;
+    private Double maf;
+    private Double maxMaf;
+    private Double nonRefAf;
+    private Double maxNonRefAf;
+    private Integer mac;
+    private Integer maxMac;
+    private Double nonRefAc;
+    private Double maxNonRefAc;
+    private Double hwe;
+    private Double geno;
+    private Integer maxMissingCound;
+    private Integer minAlleles;
+    private Integer maxAlleles;
+    private Integer thin;
     private String mask;
     private String invertMask;
-    private int maskMin;
+    private Integer maskMin;
 
     /* Individual filters */
     private ArrayList<String> keepIndv;
     private String keepIndvFile;
     private ArrayList<String> removeIndv;
     private String removeIndvFile;
-    private double minIndvMeanDp;
-    private double maxIndvMeanDp;
-    private double mind;
-    private boolean phased;
-    private int maxIndv;
-
+    private Double minIndvMeanDp;
+    private Double maxIndvMeanDp;
+    private Double mind;
+    private Boolean phased;
+    private Integer maxIndv;
+    
     /* Statistics */
-    private boolean count;
-    private boolean freq;
-    private boolean depth;
+    private Boolean count;
+    private Boolean freq;
+    private Boolean depth;
 
     /**
      * This constructor will set defaultConfig.xml as configuration file and load it.
      */
     public Settings() {
-        //this.configFile = "defaultConfig.xml";
+        this.configFile = "defaultConfig.xml";
         //this.load();
     }
 
@@ -105,7 +105,7 @@ public class Settings {
      * @param filename Filename to set as configuration source
      */
     public Settings(String filename) {
-        //this.configFile = filename;
+        this.configFile = filename;
         //this.load();
     }
 
@@ -119,17 +119,100 @@ public class Settings {
     /**
      * Load given settings file into memory.
      *
+     * @author Sergio Bondietti <sergio@bondietti.nl>
      * @param filename settings XML filename.
      */
     public void load(String filename) {
         try {
+            // open config object
             this.configRead = new XMLConfiguration(filename);
+//     
+//            
+//            
+//            // get properties
+//            /* Basic Settings */
+//            this.inputFile = configRead.getString("basic.inputFile");
+//            this.outputFile = configRead.getString("basic.outputFile");
+//            this.gzipped = configRead.getBoolean("basic.gzipped", false);
+//            
+//
+//            
+//            /* Site Filters */
+//            
+//    
+//            
+//            
+//            this.chr = configRead.getList("siteFilters.chr").;
+//            this.notChr = configRead.getList("siteFilters.notChr");
+//            this.fromBp = configRead.getInt("siteFilters.fromBp");
+//            this.toBp = configRead.getInt("siteFilters.toBp");
+//            this.snp = configRead.getList("siteFilters.snp");
+//            this.snpFile = configRead.getString("siteFilters.snpFile");
+//            this.excludeSnp =configRead.getList("siteFilters.excludeSnp");
+//            this.excludeSnpFile = configRead.getString("siteFilters.excludeSnpFile");
+//            this.positions = configRead.getList("siteFilters.positions");
+//            this.positionsFile = configRead.getString("siteFilters.positionsFile");
+//            this.excludePositions = configRead.getList("siteFilters.excludePositions");
+//            this.excludePositionsFile = configRead.getString("siteFilters.excludePositionsFile");
+//
+//            configRead.getInt(mask);
+//            configRead.getDouble(mask);
+//            configRead.getString(mask);
+//            configRead.getBoolean(mask, depth);
+//            configRead.getList(mask);
+//            
+//            
+//            this.keepOnlyIndels = configRead.getBoolean("siteFilters.keepOnlyIndels", false);
+//            this.removeIndels = configRead.getBoolean("siteFilters.removeIndels", false);
+//            this.bedFile = configRead.getString("siteFilters.bedFile");
+//            
+//
+//            // todo: implement a way to store needed bed data to xml
+//            configCreate.addProperty("siteFilters.removeFilteredAll", this.removeFilteredAll);
+//            configCreate.addProperty("siteFilters.removeFiltered", this.removeFiltered);
+//            configCreate.addProperty("siteFilters.keepFiltered", this.keepFiltered);
+//            configCreate.addProperty("siteFilters.removeInfo", this.removeInfo);
+//            configCreate.addProperty("siteFilters.keepInfo", this.keepInfo);
+//            configCreate.addProperty("siteFilters.minQ", this.minQ);
+//            configCreate.addProperty("siteFilters.minMeanDp", this.minMeanDp);
+//            configCreate.addProperty("siteFilters.maxMeanDp", this.maxMeanDp);
+//            configCreate.addProperty("siteFilters.maf", this.maf);
+//            configCreate.addProperty("siteFilters.maxMaf", this.maf);
+//            configCreate.addProperty("siteFilters.nonRefAf", this.nonRefAf);
+//            configCreate.addProperty("siteFilters.maxNonRefAf", this.maxNonRefAf);
+//            configCreate.addProperty("siteFilters.mac", this.mac);
+//            configCreate.addProperty("siteFilters.maxMac", this.maxMac);
+//            configCreate.addProperty("siteFilters.maxNonRefAc", this.maxNonRefAc);
+//            configCreate.addProperty("siteFilters.hwe", this.hwe);
+//            configCreate.addProperty("siteFilters.geno", this.geno);
+//            configCreate.addProperty("siteFilters.maxMissingCount", this.maxMissingCound);
+//            configCreate.addProperty("siteFilters.minAlleles", this.minAlleles);
+//            configCreate.addProperty("siteFilters.maxAlleles", this.maxAlleles);
+//            configCreate.addProperty("siteFilters.thin", this.thin);
+//            configCreate.addProperty("siteFilters.mask", this.mask);
+//            configCreate.addProperty("siteFilters.invertMask", this.invertMask);
+//            configCreate.addProperty("siteFilters.maskMin", this.invertMask);
+//            /* Individual filters */
+//            configCreate.addProperty("individualFilters.keepIndv", this.keepIndv);
+//            configCreate.addProperty("individualFilters.keepIndvFile", this.keepIndvFile);
+//            configCreate.addProperty("individualFilters.removeIndv", this.removeIndv);
+//            configCreate.addProperty("individualFilters.removeIndvFile", this.removeIndvFile);
+//            configCreate.addProperty("individualFilters.minIndvMeanDp", this.minIndvMeanDp);
+//            configCreate.addProperty("individualFilters.maxIndvMeanDp", this.maxIndvMeanDp);
+//            configCreate.addProperty("individualFilters.mind", this.mind);
+//            configCreate.addProperty("individualFilters.phased", this.phased);
+//            configCreate.addProperty("individualFilters.maxIndv", this.maxIndv);
+//            /* Statistics */
+//            configCreate.addProperty("statistics.count", this.count);
+//            configCreate.addProperty("statistics.freq", this.freq);
+//            configCreate.addProperty("statistics.depth", this.depth);            
             
-
-            // add other load logic here
         } catch (ConfigurationException ex) {
             Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        
+        
 
     }
 
@@ -153,7 +236,7 @@ public class Settings {
             /* Basic Settings */
             configCreate.addProperty("basic.inputFile", this.inputFile);
             configCreate.addProperty("basic.outputFile", this.outputFile);
-            configCreate.addProperty("basic.gzippedFile", this.gzipped);
+            configCreate.addProperty("basic.gzipped", this.gzipped);
             /* Site Filters */
             configCreate.addProperty("siteFilters.chr", this.chr);
             configCreate.addProperty("siteFilters.notChr", this.notChr);
@@ -227,11 +310,11 @@ public class Settings {
         this.inputFile = inputFile;
     }
 
-    public boolean isGzipped() {
+    public Boolean isGzipped() {
         return gzipped;
     }
 
-    public void setGzipped(boolean gzipped) {
+    public void setGzipped(Boolean gzipped) {
         this.gzipped = gzipped;
     }
 
@@ -259,19 +342,19 @@ public class Settings {
         this.notChr = notChr;
     }
 
-    public int getFromBp() {
+    public Integer getFromBp() {
         return fromBp;
     }
 
-    public void setFromBp(int fromBp) {
+    public void setFromBp(Integer fromBp) {
         this.fromBp = fromBp;
     }
 
-    public int getToBp() {
+    public Integer getToBp() {
         return toBp;
     }
 
-    public void setToBp(int toBp) {
+    public void setToBp(Integer toBp) {
         this.toBp = toBp;
     }
 
@@ -339,27 +422,27 @@ public class Settings {
         this.excludePositionsFile = excludePositionsFile;
     }
 
-    public boolean isKeepOnlyIndels() {
+    public Boolean isKeepOnlyIndels() {
         return keepOnlyIndels;
     }
 
-    public void setKeepOnlyIndels(boolean keepOnlyIndels) {
+    public void setKeepOnlyIndels(Boolean keepOnlyIndels) {
         this.keepOnlyIndels = keepOnlyIndels;
     }
 
-    public boolean isRemoveIndels() {
+    public Boolean isRemoveIndels() {
         return removeIndels;
     }
 
-    public void setRemoveIndels(boolean removeIndels) {
+    public void setRemoveIndels(Boolean removeIndels) {
         this.removeIndels = removeIndels;
     }
 
-    public boolean isRemoveFilteredAll() {
+    public Boolean isRemoveFilteredAll() {
         return removeFilteredAll;
     }
 
-    public void setRemoveFilteredAll(boolean removeFilteredAll) {
+    public void setRemoveFilteredAll(Boolean removeFilteredAll) {
         this.removeFilteredAll = removeFilteredAll;
     }
 
@@ -395,139 +478,139 @@ public class Settings {
         this.keepInfo = keepInfo;
     }
 
-    public double getMinQ() {
+    public Double getMinQ() {
         return minQ;
     }
 
-    public void setMinQ(double minQ) {
+    public void setMinQ(Double minQ) {
         this.minQ = minQ;
     }
 
-    public double getMinMeanDP() {
+    public Double getMinMeanDP() {
         return minMeanDp;
     }
 
-    public void setMinMeanDP(double minMeanDP) {
+    public void setMinMeanDP(Double minMeanDP) {
         this.minMeanDp = minMeanDP;
     }
 
-    public double getMaxMeanDP() {
+    public Double getMaxMeanDP() {
         return maxMeanDp;
     }
 
-    public void setMaxMeanDP(double maxMeanDP) {
+    public void setMaxMeanDP(Double maxMeanDP) {
         this.maxMeanDp = maxMeanDP;
     }
 
-    public double getMaf() {
+    public Double getMaf() {
         return maf;
     }
 
-    public void setMaf(double maf) {
+    public void setMaf(Double maf) {
         this.maf = maf;
     }
 
-    public double getMaxMaf() {
+    public Double getMaxMaf() {
         return maxMaf;
     }
 
-    public void setMaxMaf(double maxMaf) {
+    public void setMaxMaf(Double maxMaf) {
         this.maxMaf = maxMaf;
     }
 
-    public double getNonRefAf() {
+    public Double getNonRefAf() {
         return nonRefAf;
     }
 
-    public void setNonRefAf(double nonRefAf) {
+    public void setNonRefAf(Double nonRefAf) {
         this.nonRefAf = nonRefAf;
     }
 
-    public double getMaxNonRefAf() {
+    public Double getMaxNonRefAf() {
         return maxNonRefAf;
     }
 
-    public void setMaxNonRefAf(double maxNonRefAf) {
+    public void setMaxNonRefAf(Double maxNonRefAf) {
         this.maxNonRefAf = maxNonRefAf;
     }
 
-    public int getMac() {
+    public Integer getMac() {
         return mac;
     }
 
-    public void setMac(int mac) {
+    public void setMac(Integer mac) {
         this.mac = mac;
     }
 
-    public int getMaxMac() {
+    public Integer getMaxMac() {
         return maxMac;
     }
 
-    public void setMaxMac(int maxMac) {
+    public void setMaxMac(Integer maxMac) {
         this.maxMac = maxMac;
     }
 
-    public double getNonRefAc() {
+    public Double getNonRefAc() {
         return nonRefAc;
     }
 
-    public void setNonRefAc(double nonRefAc) {
+    public void setNonRefAc(Double nonRefAc) {
         this.nonRefAc = nonRefAc;
     }
 
-    public double getMaxNonRefAc() {
+    public Double getMaxNonRefAc() {
         return maxNonRefAc;
     }
 
-    public void setMaxNonRefAc(double maxNonRefAc) {
+    public void setMaxNonRefAc(Double maxNonRefAc) {
         this.maxNonRefAc = maxNonRefAc;
     }
 
-    public double getHwe() {
+    public Double getHwe() {
         return hwe;
     }
 
-    public void setHwe(double hwe) {
+    public void setHwe(Double hwe) {
         this.hwe = hwe;
     }
 
-    public double getGeno() {
+    public Double getGeno() {
         return geno;
     }
 
-    public void setGeno(double geno) {
+    public void setGeno(Double geno) {
         this.geno = geno;
     }
 
-    public int getMaxMissingCound() {
+    public Integer getMaxMissingCound() {
         return maxMissingCound;
     }
 
-    public void setMaxMissingCound(int maxMissingCound) {
+    public void setMaxMissingCound(Integer maxMissingCound) {
         this.maxMissingCound = maxMissingCound;
     }
 
-    public int getMinAlleles() {
+    public Integer getMinAlleles() {
         return minAlleles;
     }
 
-    public void setMinAlleles(int minAlleles) {
+    public void setMinAlleles(Integer minAlleles) {
         this.minAlleles = minAlleles;
     }
 
-    public int getMaxAlleles() {
+    public Integer getMaxAlleles() {
         return maxAlleles;
     }
 
-    public void setMaxAlleles(int maxAlleles) {
+    public void setMaxAlleles(Integer maxAlleles) {
         this.maxAlleles = maxAlleles;
     }
 
-    public int getThin() {
+    public Integer getThin() {
         return thin;
     }
 
-    public void setThin(int thin) {
+    public void setThin(Integer thin) {
         this.thin = thin;
     }
 
@@ -547,11 +630,11 @@ public class Settings {
         this.invertMask = invertMask;
     }
 
-    public int getMaskMin() {
+    public Integer getMaskMin() {
         return maskMin;
     }
 
-    public void setMaskMin(int maskMin) {
+    public void setMaskMin(Integer maskMin) {
         this.maskMin = maskMin;
     }
 
@@ -587,67 +670,67 @@ public class Settings {
         this.removeIndvFile = removeIndvFile;
     }
 
-    public double getMinIndvMeanDp() {
+    public Double getMinIndvMeanDp() {
         return minIndvMeanDp;
     }
 
-    public void setMinIndvMeanDp(double minIndvMeanDp) {
+    public void setMinIndvMeanDp(Double minIndvMeanDp) {
         this.minIndvMeanDp = minIndvMeanDp;
     }
 
-    public double getMaxIndvMeanDp() {
+    public Double getMaxIndvMeanDp() {
         return maxIndvMeanDp;
     }
 
-    public void setMaxIndvMeanDp(double maxIndvMeanDp) {
+    public void setMaxIndvMeanDp(Double maxIndvMeanDp) {
         this.maxIndvMeanDp = maxIndvMeanDp;
     }
 
-    public double getMind() {
+    public Double getMind() {
         return mind;
     }
 
-    public void setMind(double mind) {
+    public void setMind(Double mind) {
         this.mind = mind;
     }
 
-    public boolean isPhased() {
+    public Boolean isPhased() {
         return phased;
     }
 
-    public void setPhased(boolean phased) {
+    public void setPhased(Boolean phased) {
         this.phased = phased;
     }
 
-    public int getMaxIndv() {
+    public Integer getMaxIndv() {
         return maxIndv;
     }
 
-    public void setMaxIndv(int maxIndv) {
+    public void setMaxIndv(Integer maxIndv) {
         this.maxIndv = maxIndv;
     }
 
-    public boolean isCount() {
+    public Boolean isCount() {
         return count;
     }
 
-    public void setCount(boolean count) {
+    public void setCount(Boolean count) {
         this.count = count;
     }
 
-    public boolean isFreq() {
+    public Boolean isFreq() {
         return freq;
     }
 
-    public void setFreq(boolean freq) {
+    public void setFreq(Boolean freq) {
         this.freq = freq;
     }
 
-    public boolean isDepth() {
+    public Boolean isDepth() {
         return depth;
     }
 
-    public void setDepth(boolean depth) {
+    public void setDepth(Boolean depth) {
         this.depth = depth;
     }
 
