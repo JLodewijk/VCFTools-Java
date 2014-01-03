0321e21411b7ef8b76653ddb82d576bc190a686e
Jeroen <Jeroen@Laptop_Jeroen>
2014-01-03 20:05:06 +0100

    Added a line of documentation.

--- VcftoolsWeb/src/java/nl/bioinf/vcftools/vcftools_web/servlets/LoginServlet.java
+++ VcftoolsWeb/src/java/nl/bioinf/vcftools/vcftools_web/servlets/LoginServlet.java
@@ -12,6 +12,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        //Get user information
         String name = request.getParameter("username");
         String pass = request.getParameter("password");
         if (ValidateUser.checkUserInformation(name, pass)) {
