740b6782612a96f63be55a2e3d59cf29b52cbea0
Jeroen Lodewijk <j.lodewijk@st.hanze.nl>
2014-01-04 13:24:28 +0100

    LoginServlet can now see which button was pressed on the form. Added support for registration as well. Updated the welcome page

--- VcftoolsWeb/src/java/nl/bioinf/vcftools/vcftools_web/servlets/LoginServlet.java
+++ VcftoolsWeb/src/java/nl/bioinf/vcftools/vcftools_web/servlets/LoginServlet.java
@@ -12,16 +12,31 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //Get user information
+        // Gets the value of the pressed button.
+        String userinput = request.getParameter("userinput");
         String name = request.getParameter("username");
         String pass = request.getParameter("password");
-        if (ValidateUser.checkUserInformation(name, pass)) {
-            RequestDispatcher rs = request.getRequestDispatcher("welcome.html");
-            rs.forward(request, response);
-        } else {
-            RequestDispatcher rs = request.getRequestDispatcher("login.jsp");
-            rs.forward(request, response);
-        }
+        // If user presses the button login
+        if (userinput.equals("Login")) {
+            if (ValidateUser.checkUserInformation(name, pass)) {
+                RequestDispatcher rs = request.getRequestDispatcher("welcome.html");
+                rs.forward(request, response);
+            } else {
+                RequestDispatcher rs = request.getRequestDispatcher("login.jsp");
+                rs.forward(request, response);
+            }
 
+        }// Else user pressed the register button.
+        else {
+            // To prevent duplicate usernames, since the usernames are the primary key.
+            if (ValidateUser.doesUserNameExist(name)) {
+                RequestDispatcher rs = request.getRequestDispatcher("error.html");
+                rs.forward(request, response);
+            } else {
+                CreateUser.insertNewUser(name, pass);
+                RequestDispatcher rs = request.getRequestDispatcher("welcome.html");
+                rs.forward(request, response);
+            }
+        }
     }
 }
