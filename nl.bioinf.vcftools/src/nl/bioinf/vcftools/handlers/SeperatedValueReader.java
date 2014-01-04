9ff47c01cfe59e80eb7cbb1d7ea412560d7c242c
Sergio <Sergio@notebook>
2014-01-04 11:38:40 +0100

    added separated value reader to handlers package

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/handlers/SeperatedValueReader.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/handlers/SeperatedValueReader.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nl.bioinf.vcftools;
+package nl.bioinf.vcftools.handlers;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
