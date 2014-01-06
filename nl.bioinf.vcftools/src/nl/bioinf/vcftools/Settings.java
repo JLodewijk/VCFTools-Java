0e7f9bc9c841be9c9a1ec6d297fb9a2b04de4114
sbondietti <sbondietti@bin257.bin>
2014-01-06 15:39:06 +0100

    Fixed javadoc base in settings

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/Settings.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/Settings.java
@@ -271,126 +271,257 @@ public class Settings {
     }
 
     /* Getters and setters for the VCFTools settings */
+
+    /**
+     *
+     * @return
+     */
+    
     public String getConfigFile() {
         return configFile;
     }
 
+    /**
+     *
+     * @param configFile
+     */
     public void setConfigFile(String configFile) {
         this.configFile = configFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getInputFile() {
         return inputFile;
     }
 
+    /**
+     *
+     * @param inputFile
+     */
     public void setInputFile(String inputFile) {
         this.inputFile = inputFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getOutputFile() {
         return outputFile;
     }
 
+    /**
+     *
+     * @param outputFile
+     */
     public void setOutputFile(String outputFile) {
         this.outputFile = outputFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public Boolean isGzipped() {
         return gzipped;
     }
 
+    /**
+     *
+     * @param gzipped
+     */
     public void setGzipped(Boolean gzipped) {
         this.gzipped = gzipped;
     }
 
+    /**
+     *
+     * @return
+     */
     public MultiMap getChr() {
         return chr;
     }
 
+    /**
+     *
+     * @param chr
+     */
     public void setChr(MultiMap chr) {
         this.chr = chr;
     }
 
+    /**
+     *
+     * @param chr
+     */
     public void addChr(String chr) {
         this.chr.put(chr, null);
     }    
 
+    /**
+     *
+     * @param chr
+     * @param fromBp
+     * @param toBp
+     */
     public void addChr(String chr, int fromBp, int toBp) {
         this.chr.put(chr, Arrays.asList(fromBp, toBp));
-    } 
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public MultiMap getNotChr() {
         return notChr;
     }
 
+    /**
+     *
+     * @param notChr
+     */
     public void setNotChr(MultiMap notChr) {
         this.notChr = notChr;
     }
 
+    /**
+     *
+     * @param chr
+     */
     public void addNotChr(String chr) {
         this.notChr.put(chr, null);
     }    
 
+    /**
+     *
+     * @param chr
+     * @param fromBp
+     * @param toBp
+     */
     public void addNotChr(String chr, int fromBp, int toBp) {
         this.notChr.put(chr, Arrays.asList(fromBp, toBp));
     } 
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<String> getSnp() {
         return snp;
     }
 
+    /**
+     *
+     * @param snp
+     */
     public void setSnp(ArrayList<String> snp) {
         this.snp = snp;
     }
 
+    /**
+     *
+     * @param snp
+     */
     public void addSnp(ArrayList<String> snp) {
         this.snp.addAll(snp);
-    }    
-    
+    }
+
+    /**
+     *
+     * @param snp
+     */
     public void addSnp(String snp) {
         this.snp.add(snp);
-    }    
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public String getSnpFile() {
         return snpFile;
     }
 
+    /**
+     *
+     * @param snpFile
+     */
     public void setSnpFile(String snpFile) {
         this.snpFile = snpFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<String> getExcludeSnp() {
         return excludeSnp;
     }
 
+    /**
+     *
+     * @param excludeSnp
+     */
     public void setExcludeSnp(ArrayList<String> excludeSnp) {
         this.excludeSnp = excludeSnp;
     }
 
+    /**
+     *
+     * @param excludeSnp
+     */
     public void addExcludeSnp(ArrayList<String> excludeSnp) {
         this.excludeSnp.addAll(excludeSnp);
-    }   
-    
+    }
+
+    /**
+     *
+     * @param excludeSnp
+     */
     public void addExcludeSnp(String excludeSnp) {
         this.excludeSnp.add(excludeSnp);
-    } 
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public String getExcludeSnpFile() {
         return excludeSnpFile;
     }
 
+    /**
+     *
+     * @param excludeSnpFile
+     */
     public void setExcludeSnpFile(String excludeSnpFile) {
         this.excludeSnpFile = excludeSnpFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public MultiMap getPositions() {
         return positions;
     }
 
+    /**
+     *
+     * @param positions
+     */
     public void setPositions(MultiMap positions) {
         this.positions = positions;
     }
 
+    /**
+     *
+     * @param key
+     * @param value
+     */
     public void addPositions(String key, Integer value) {
         this.positions.put(key, value);
     }
@@ -406,22 +537,43 @@ public class Settings {
         return chrPos.contains(position);
     }
     
+    /**
+     *
+     * @return
+     */
     public String getPositionsFile() {
         return positionsFile;
     }
 
+    /**
+     *
+     * @param positionsFile
+     */
     public void setPositionsFile(String positionsFile) {
         this.positionsFile = positionsFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public MultiMap getExcludePositions() {
         return excludePositions;
     }
 
+    /**
+     *
+     * @param excludePositions
+     */
     public void setExcludePositions(MultiMap excludePositions) {
         this.excludePositions = excludePositions;
     }
 
+    /**
+     *
+     * @param key
+     * @param value
+     */
     public void addExcludePositions(String key, Integer value) {
         this.excludePositions.put(key, value);
     }    
@@ -435,381 +587,756 @@ public class Settings {
     public boolean containsExcludePositions(String chr, int position) {
         ArrayList chrPos = (ArrayList) this.excludePositions.get(chr);
         return chrPos.contains(position);
-    }    
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public String getExcludePositionsFile() {
         return excludePositionsFile;
     }
 
+    /**
+     *
+     * @param excludePositionsFile
+     */
     public void setExcludePositionsFile(String excludePositionsFile) {
         this.excludePositionsFile = excludePositionsFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public Boolean isKeepIndels() {
         return keepIndels;
     }
 
+    /**
+     *
+     * @param keepIndels
+     */
     public void setKeepIndels(Boolean keepIndels) {
         this.keepIndels = keepIndels;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getBedFile() {
         return bedFile;
     }
 
+    /**
+     *
+     * @param bedFile
+     */
     public void setBedFile(String bedFile) {
         this.bedFile = bedFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getExludeBedFile() {
         return exludeBedFile;
     }
 
+    /**
+     *
+     * @param exludeBedFile
+     */
     public void setExludeBedFile(String exludeBedFile) {
         this.exludeBedFile = exludeBedFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public Boolean isRemoveFilteredAll() {
         return removeFilteredAll;
     }
 
+    /**
+     *
+     * @param removeFilteredAll
+     */
     public void setRemoveFilteredAll(Boolean removeFilteredAll) {
         this.removeFilteredAll = removeFilteredAll;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<String> getRemoveFiltered() {
         return removeFiltered;
     }
 
+    /**
+     *
+     * @param removeFiltered
+     */
     public void setRemoveFiltered(ArrayList<String> removeFiltered) {
         this.removeFiltered = removeFiltered;
     }
 
+    /**
+     *
+     * @param removeFiltered
+     */
     public void addRemoveFiltered(ArrayList<String> removeFiltered) {
         this.removeFiltered.addAll(removeFiltered);
-    }    
-    
+    }
+
+    /**
+     *
+     * @param removeFiltered
+     */
     public void addRemoveFiltered(String removeFiltered) {
         this.removeFiltered.add(removeFiltered);
-    }    
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public ArrayList<String> getKeepFiltered() {
         return keepFiltered;
     }
 
+    /**
+     *
+     * @param keepFiltered
+     */
     public void setKeepFiltered(ArrayList<String> keepFiltered) {
         this.keepFiltered = keepFiltered;
     }
 
+    /**
+     *
+     * @param keepFiltered
+     */
     public void addKeepFiltered(ArrayList<String> keepFiltered) {
         this.keepFiltered.addAll(keepFiltered);
-    }    
-    
+    }
+
+    /**
+     *
+     * @param keepFiltered
+     */
     public void addKeepFiltered(String keepFiltered) {
         this.keepFiltered.add(keepFiltered);
-    }    
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public ArrayList<String> getRemoveInfo() {
         return removeInfo;
     }
 
+    /**
+     *
+     * @param removeInfo
+     */
     public void setRemoveInfo(ArrayList<String> removeInfo) {
         this.removeInfo = removeInfo;
     }
 
+    /**
+     *
+     * @param removeInfo
+     */
     public void addRemoveInfo(ArrayList<String> removeInfo) {
         this.removeInfo.addAll(removeInfo);
-    }    
-    
+    }
+
+    /**
+     *
+     * @param removeInfo
+     */
     public void addRemoveInfo(String removeInfo) {
         this.removeInfo.add(removeInfo);
-    }    
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public ArrayList<String> getKeepInfo() {
         return keepInfo;
     }
 
+    /**
+     *
+     * @param keepInfo
+     */
     public void setKeepInfo(ArrayList<String> keepInfo) {
         this.keepInfo = keepInfo;
     }
 
+    /**
+     *
+     * @param keepInfo
+     */
     public void addKeepInfo(ArrayList<String> keepInfo) {
         this.keepInfo.addAll(keepInfo);
     }
     
+    /**
+     *
+     * @param keepInfo
+     */
     public void addKeepInfo(String keepInfo) {
         this.keepInfo.add(keepInfo);
-    }   
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public Double getMinQ() {
         return minQ;
     }
 
+    /**
+     *
+     * @param minQ
+     */
     public void setMinQ(Double minQ) {
         this.minQ = minQ;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMinMeanDp() {
         return minMeanDp;
     }
 
+    /**
+     *
+     * @param minMeanDp
+     */
     public void setMinMeanDp(Double minMeanDp) {
         this.minMeanDp = minMeanDp;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMaxMeanDp() {
         return maxMeanDp;
     }
 
+    /**
+     *
+     * @param maxMeanDp
+     */
     public void setMaxMeanDp(Double maxMeanDp) {
         this.maxMeanDp = maxMeanDp;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMaf() {
         return maf;
     }
 
+    /**
+     *
+     * @param maf
+     */
     public void setMaf(Double maf) {
         this.maf = maf;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMaxMaf() {
         return maxMaf;
     }
 
+    /**
+     *
+     * @param maxMaf
+     */
     public void setMaxMaf(Double maxMaf) {
         this.maxMaf = maxMaf;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getNonRefAf() {
         return nonRefAf;
     }
 
+    /**
+     *
+     * @param nonRefAf
+     */
     public void setNonRefAf(Double nonRefAf) {
         this.nonRefAf = nonRefAf;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMaxNonRefAf() {
         return maxNonRefAf;
     }
 
+    /**
+     *
+     * @param maxNonRefAf
+     */
     public void setMaxNonRefAf(Double maxNonRefAf) {
         this.maxNonRefAf = maxNonRefAf;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getMac() {
         return mac;
     }
 
+    /**
+     *
+     * @param mac
+     */
     public void setMac(Integer mac) {
         this.mac = mac;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getMaxMac() {
         return maxMac;
     }
 
+    /**
+     *
+     * @param maxMac
+     */
     public void setMaxMac(Integer maxMac) {
         this.maxMac = maxMac;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getNonRefAc() {
         return nonRefAc;
     }
 
+    /**
+     *
+     * @param nonRefAc
+     */
     public void setNonRefAc(Double nonRefAc) {
         this.nonRefAc = nonRefAc;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMaxNonRefAc() {
         return maxNonRefAc;
     }
 
+    /**
+     *
+     * @param maxNonRefAc
+     */
     public void setMaxNonRefAc(Double maxNonRefAc) {
         this.maxNonRefAc = maxNonRefAc;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getHwe() {
         return hwe;
     }
 
+    /**
+     *
+     * @param hwe
+     */
     public void setHwe(Double hwe) {
         this.hwe = hwe;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getGeno() {
         return geno;
     }
 
+    /**
+     *
+     * @param geno
+     */
     public void setGeno(Double geno) {
         this.geno = geno;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getMaxMissingCount() {
         return maxMissingCount;
     }
 
+    /**
+     *
+     * @param maxMissingCount
+     */
     public void setMaxMissingCount(Integer maxMissingCount) {
         this.maxMissingCount = maxMissingCount;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getMinAlleles() {
         return minAlleles;
     }
 
+    /**
+     *
+     * @param minAlleles
+     */
     public void setMinAlleles(Integer minAlleles) {
         this.minAlleles = minAlleles;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getMaxAlleles() {
         return maxAlleles;
     }
 
+    /**
+     *
+     * @param maxAlleles
+     */
     public void setMaxAlleles(Integer maxAlleles) {
         this.maxAlleles = maxAlleles;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getThin() {
         return thin;
     }
 
+    /**
+     *
+     * @param thin
+     */
     public void setThin(Integer thin) {
         this.thin = thin;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getMask() {
         return mask;
     }
 
+    /**
+     *
+     * @param mask
+     */
     public void setMask(String mask) {
         this.mask = mask;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getInvertMask() {
         return invertMask;
     }
 
+    /**
+     *
+     * @param invertMask
+     */
     public void setInvertMask(String invertMask) {
         this.invertMask = invertMask;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getMaskMin() {
         return maskMin;
     }
 
+    /**
+     *
+     * @param maskMin
+     */
     public void setMaskMin(Integer maskMin) {
         this.maskMin = maskMin;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<String> getKeepIndv() {
         return keepIndv;
     }
 
+    /**
+     *
+     * @param keepIndv
+     */
     public void setKeepIndv(ArrayList<String> keepIndv) {
         this.keepIndv = keepIndv;
     }
 
+    /**
+     *
+     * @param keepIndv
+     */
     public void addKeepIndv(ArrayList<String> keepIndv) {
         this.keepIndv.addAll(keepIndv);
     }
     
+    /**
+     *
+     * @param keepIndv
+     */
     public void addKeepIndv(String keepIndv) {
         this.keepIndv.add(keepIndv);
-    }    
-    
+    }
+
+    /**
+     *
+     * @return
+     */
     public String getKeepIndvFile() {
         return keepIndvFile;
     }
 
+    /**
+     *
+     * @param keepIndvFile
+     */
     public void setKeepIndvFile(String keepIndvFile) {
         this.keepIndvFile = keepIndvFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<String> getRemoveIndv() {
         return removeIndv;
     }
 
+    /**
+     *
+     * @param removeIndv
+     */
     public void setRemoveIndv(ArrayList<String> removeIndv) {
         this.removeIndv = removeIndv;
     }
 
+    /**
+     *
+     * @param removeIndv
+     */
     public void addRemoveIndv(ArrayList<String> removeIndv) {
         this.removeIndv.addAll(removeIndv);
     }
     
+    /**
+     *
+     * @param removeIndv
+     */
     public void addRemoveIndv(String removeIndv) {
         this.removeIndv.add(removeIndv);
     }    
 
-    
+    /**
+     *
+     * @return
+     */
     public String getRemoveIndvFile() {
         return removeIndvFile;
     }
 
+    /**
+     *
+     * @param removeIndvFile
+     */
     public void setRemoveIndvFile(String removeIndvFile) {
         this.removeIndvFile = removeIndvFile;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMinIndvMeanDp() {
         return minIndvMeanDp;
     }
 
+    /**
+     *
+     * @param minIndvMeanDp
+     */
     public void setMinIndvMeanDp(Double minIndvMeanDp) {
         this.minIndvMeanDp = minIndvMeanDp;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMaxIndvMeanDp() {
         return maxIndvMeanDp;
     }
 
+    /**
+     *
+     * @param maxIndvMeanDp
+     */
     public void setMaxIndvMeanDp(Double maxIndvMeanDp) {
         this.maxIndvMeanDp = maxIndvMeanDp;
     }
 
+    /**
+     *
+     * @return
+     */
     public Double getMind() {
         return mind;
     }
 
+    /**
+     *
+     * @param mind
+     */
     public void setMind(Double mind) {
         this.mind = mind;
     }
 
+    /**
+     *
+     * @return
+     */
     public Boolean isPhased() {
         return phased;
     }
 
+    /**
+     *
+     * @param phased
+     */
     public void setPhased(Boolean phased) {
         this.phased = phased;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getMaxIndv() {
         return maxIndv;
     }
 
+    /**
+     *
+     * @param maxIndv
+     */
     public void setMaxIndv(Integer maxIndv) {
         this.maxIndv = maxIndv;
     }
 
+    /**
+     *
+     * @return
+     */
     public Boolean isCount() {
         return count;
     }
 
+    /**
+     *
+     * @param count
+     */
     public void setCount(Boolean count) {
         this.count = count;
     }
 
+    /**
+     *
+     * @return
+     */
     public Boolean isFreq() {
         return freq;
     }
 
+    /**
+     *
+     * @param freq
+     */
     public void setFreq(Boolean freq) {
         this.freq = freq;
     }
 
+    /**
+     *
+     * @return
+     */
     public Boolean isDepth() {
         return depth;
     }
 
+    /**
+     *
+     * @param depth
+     */
     public void setDepth(Boolean depth) {
         this.depth = depth;
     }
