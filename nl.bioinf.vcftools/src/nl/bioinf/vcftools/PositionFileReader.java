6e7e6ff993813103fbf4ecfe732d132a6708743c
Ashvin <Ashvin@145.37.88.199>
2014-01-06 16:11:01 +0100

    - PositionFileReader.java added

---
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/PositionFileReader.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.bioinf.vcftools;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.apache.commons.collections4.MapIterator;
+import org.apache.commons.collections4.MultiMap;
+import org.apache.commons.collections4.map.MultiValueMap;
+
+/**
+ *
+ * @author Ashvin
+ */
+public class PositionFileReader {
+   
+    public MultiMap bleep(){
+     MultiMap chrPositionMap = new MultiValueMap();
+     
+        
+        
+        return null;
+    
+    
+    }
+    
+    
+}
