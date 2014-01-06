3f489c83059b193f98f26cd3254d6a261085edbd
mhroelfes <mhroelfes@bin256.bin>
2014-01-06 15:56:35 +0100

    added Include and exclude snp

---
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/ExcludeSnp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.bioinf.vcftools.filters;
+
+import nl.bioinf.vcftools.Settings;
+import nl.bioinf.vcftools.handlers.VcfLine;
+
+/**
+ *
+ * @author mhroelfes
+ */
+public class ExcludeSnp extends AbstractSimpleFilter{
+
+    @Override
+    public boolean filter(VcfLine vcfLine, Settings settings) {
+        return settings.getExcludeSnp().contains(vcfLine.getId());
+    }
+    
+}
