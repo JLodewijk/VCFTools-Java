371a23ee312dcbe2164ee5ff7b50613e1b64e512
mhroelfes <mhroelfes@bin256.bin>
2014-01-06 15:25:00 +0100

    added IncludePositions

---
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/IncludePositions.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.bioinf.vcftools.filters;
+
+import java.util.ArrayList;
+import nl.bioinf.vcftools.Settings;
+import nl.bioinf.vcftools.handlers.VcfLine;
+import org.apache.commons.collections4.MultiMap;
+import org.apache.commons.collections4.map.MultiValueMap;
+
+/**
+ *
+ * @author mhroelfes <marcoroelfes@gmail.com>
+ */
+public class IncludePositions extends AbstractSimpleFilter{
+
+    @Override
+    public boolean filter(VcfLine vcfLine, Settings settings) {
+            return settings.containsPostitions(vcfLine.getChr(), vcfLine.getPosition());
+    }
+
+    
+}
--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/NonRefAlleleFrequencies.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/NonRefAlleleFrequencies.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package nl.bioinf.vcftools.filters;
 
 import java.util.ArrayList;
@@ -15,51 +14,50 @@ import nl.bioinf.vcftools.handlers.VcfLine;
  *
  * @author mhroelfes
  */
-public class NonRefAlleleFrequencies extends AbstractSimpleFilter{
+public class NonRefAlleleFrequencies extends AbstractSimpleFilter {
 
     @Override
     public boolean filter(VcfLine vcfLine, Settings settings) {
-        
-            Object valObj = vcfLine.getAttributeAsDouble("MLEAF");
-            boolean reject = false;
-            //If MLEAF contains only one value then perform this segment. 
-            if (valObj instanceof String) {
-                //Handles a single val
-                Double val = Double.valueOf((String) valObj);
-                //if val is between threshold (minRefAlleleFreq and maxRefAlleleFreq) approve line, else reject line.
-                if (val < settings.getMaxNonRefAf()&& val > settings.getNonRefAf()) {
-                   return true;
-                } else {
-                    return false;
-                }
-            } //Sometimes a MLEAF contains multipule MLEAF values instead of one
-            else {
-                boolean keep = true;
-//ArrayList value.
-                
-                ArrayList<String> values = (ArrayList<String>) valObj;
-                List<Double> valuesDoubles = new ArrayList<Double>();
-                //set String ArrayList to Double ArrayList.      
-                for (String str : values) {
-                    valuesDoubles.add(str != null ? Double.parseDouble(str) : null);
-                }
-                //for every double in ArrayList check if between threshold. If it falls between the thresholds then reject = false
-                for (double dValue : valuesDoubles) {
-                    if (dValue < settings.getMaxNonRefAf() && dValue > settings.getNonRefAf()) {
-                        keep = true;
-                    } else {
-                        //Does not fall between the thresholds, so break out of loop. Since the SNP is rejected anyway
-                        keep = false;
-                        break;
 
-                    }
-                }
-
-                //check if to reject or approve SNP
-                return keep;
+        Object valObj = vcfLine.getAttributeAsDouble("MLEAF");
+        boolean reject = false;
+        //If MLEAF contains only one value then perform this segment. 
+        if (valObj instanceof String) {
+            //Handles a single val
+            Double val = Double.valueOf((String) valObj);
+            //if val is between threshold (minRefAlleleFreq and maxRefAlleleFreq) approve line, else reject line.
+            if (val < settings.getMaxNonRefAf() && val > settings.getNonRefAf()) {
+                return true;
+            } else {
+                return false;
             }
-        
-        
+        } //Sometimes a MLEAF contains multipule MLEAF values instead of one
+        else {
+            boolean keep = true;
+            //ArrayList value.
+
+            ArrayList<String> values = (ArrayList<String>) valObj;
+            List<Double> valuesDoubles = new ArrayList<Double>();
+            //set String ArrayList to Double ArrayList.      
+            for (String str : values) {
+                valuesDoubles.add(str != null ? Double.parseDouble(str) : null);
+            }
+            //for every double in ArrayList check if between threshold. If it falls between the thresholds then reject = false
+            for (double dValue : valuesDoubles) {
+                if (dValue < settings.getMaxNonRefAf() && dValue > settings.getNonRefAf()) {
+                    keep = true;
+                } else {
+                    //Does not fall between the thresholds, so break out of loop. Since the SNP is rejected anyway
+                    keep = false;
+                    break;
+
+                }
+            }
+
+            //check if to reject or approve SNP
+            return keep;
+        }
+
     }
-    
+
 }
