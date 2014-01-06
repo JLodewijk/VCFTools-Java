b29536e170069a0f66a43068b6ac124a94f1e387
mhroelfes <mhroelfes@bin256.bin>
2014-01-06 13:30:48 +0100

    fixed  AlleleFrequencies

---
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/AlleleFrequencies.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.bioinf.vcftools.filters;
+
+import java.util.ArrayList;
+import java.util.List;
+import nl.bioinf.vcftools.Settings;
+import nl.bioinf.vcftools.handlers.VcfLine;
+
+/**
+ *
+ * @author mhroelfes
+ */
+public class AlleleFrequencies extends AbstractSimpleFilter{
+
+    @Override
+    public boolean filter(VcfLine vcfLine, Settings settings) {
+            Object valObj = vcfLine.getAttributeAsDouble("AF");
+            //System.out.println(valObj.getClass().getName());
+            
+            if (valObj instanceof String) {
+                //handle single value
+                Double val = Double.valueOf((String) valObj);
+                //if val is between threshold approve line, else reject line
+                if (val < settings.getMaxNonRefAf()&& val > settings.getNonRefAf()) {
+                    return true;
+                } else {
+                    return false;
+                }
+                //System.out.println("val="+val);
+            } else {
+                boolean keep = true;
+                //ArrayList value
+                ArrayList<String> values = (ArrayList<String>) valObj;
+                List<Double> valuesDoubles = new ArrayList<Double>();
+                //set String ArrayList to Double ArrayList            
+                for (String str : values) {
+                    valuesDoubles.add(str != null ? Double.parseDouble(str) : null);
+                }
+                //for every double in ArrayList check if between threshold
+                for (double dValue : valuesDoubles) {
+                    if (dValue < settings.getMaxNonRefAf() && dValue > settings.getNonRefAf()) {
+                        keep = true;
+                    } else {
+                        keep = false;
+                        break;
+
+                    }
+                }
+                
+                return keep;
+
+              
+            }
+    }
+    
+}
