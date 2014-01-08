cfd04c040dbcca6b7fca363137a7506a8c09bf2e
mhroelfes <mhroelfes@bin256.bin>
2014-01-08 10:53:36 +0100

    added filteronthinning and som javadoc

---
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterOnThinning.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.bioinf.vcftools.filters;
+
+import nl.bioinf.vcftools.Settings;
+import nl.bioinf.vcftools.handlers.VcfLine;
+
+/**
+ * This class filters a vcf line based on position of the SNP, if a SNP is too close to another it returns false and the line is rejected.
+ * @author mhroelfes <marcoroelfes@gmail.com>
+ */
+public class FilterOnThinning extends AbstractSimpleFilter {
+
+    int positionPreviousSnp;
+
+    @Override
+    public boolean filter(VcfLine vcfLine, Settings settings) {
+        //check if minsnpDist is valid
+        if (settings.getThin() < 0) {
+            System.out.println("Min snp distance has to be 0 or higher");
+        }
+
+        // check if it is first line, if it is set position
+        if (this.positionPreviousSnp == 0) {
+            this.positionPreviousSnp = vcfLine.getPosition();
+            return true;
+            // if not first line check if positions are to close, if to close reject line, else approve line and set new position
+        } else {
+            if ((vcfLine.getPosition() - this.positionPreviousSnp) < settings.getThin()) {
+                return false;
+
+            } else {
+
+                this.positionPreviousSnp = settings.getThin();
+                return true;
+            }
+
+        }
+    }
+
+}
