4b41a62d21d8f32763d2703210f89111c9008b74
sbondietti <sbondietti@bin257.bin>
2014-01-08 12:02:31 +0100

    expanded FilterFactory

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterFactory.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterFactory.java
@@ -24,9 +24,26 @@ public class FilterFactory {
     
     public void createSimpleFilters() {
         this.simpleFilters = new ArrayList<>();
-        if (this.settings.getMinQ() != null) { this.simpleFilters.add(new MinimalQuality()); }
+        if (!this.settings.getChr().isEmpty()) { this.simpleFilters.add(new IncludeChromosome()); }
+        if (!this.settings.getNotChr().isEmpty()) { this.simpleFilters.add(new ExcludeChromosome()); }
+        if (!this.settings.getSnp().isEmpty()) { this.simpleFilters.add(new IncludeSnp()); }
+        if (!this.settings.getPositions().isEmpty()) { this.simpleFilters.add(new IncludePositions()); }
         if (this.settings.getKeepInfo() != null) { this.simpleFilters.add(new KeepInfo()); }
-        if (this.settings.isKeepIndels() != true) { this.simpleFilters.add(new KeepIndels()); }
+        if (this.settings.isKeepIndels() == true) { this.simpleFilters.add(new KeepIndels()); }
+        if (this.settings.isKeepIndels() == false) { this.simpleFilters.add(new RemoveIndels()); }
+        if (!this.settings.getBed().isEmpty()) { /* bed incl chromosome implementation */ }
+        if (!this.settings.getExludeBed().isEmpty()) { /* bed incl chromosome implementation */ }
+        if (this.settings.isRemoveFilteredAll() == true) { this.simpleFilters.add(new RemoveFiltered()); }
+        if (!this.settings.getRemoveFiltered().isEmpty()) { this.simpleFilters.add(new RemoveSpecificFilter()); }
+        if (!this.settings.getKeepFiltered().isEmpty()) { this.simpleFilters.add(new KeepSpecificFilter()); }
+        if (!this.settings.getRemoveInfo().isEmpty()) { this.simpleFilters.add(new RemoveInfo()); }
+        if (!this.settings.getKeepInfo().isEmpty()) { this.simpleFilters.add(new KeepInfo()); }
+        if (this.settings.getMinQ() != null) { this.simpleFilters.add(new MinimalQuality()); }
+        if ((this.settings.getMinMeanDp() != null) && (this.settings.getMaxMeanDp() != null)) { this.simpleFilters.add(new MeanDepth()); }
+        // filter
+        
+        
+
     }
 
     public ArrayList<AbstractSimpleFilter> getSimpleFilters() {
