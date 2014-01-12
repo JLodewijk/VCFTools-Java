32ea056a6dda2e4d706201aa9427352099305617
Sergio <Sergio@notebook>
2014-01-12 13:00:54 +0100

    Updated filters to work with multiple position types

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/site/ExcludeChromosome.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/site/ExcludeChromosome.java
@@ -6,19 +6,43 @@
 
 package nl.bioinf.vcftools.filters.site;
 
+import java.util.Collection;
+import java.util.List;
 import nl.bioinf.vcftools.Settings;
 import nl.bioinf.vcftools.filters.AbstractSimpleFilter;
 import nl.bioinf.vcftools.handlers.VcfLine;
 
 /**
  *
- * @author Jeroen Lodewijk <j.lodewijk@st.hanze.nl>
+ * @author Sergio Bondietti <sergio@bondietti.nl>
  */
 public class ExcludeChromosome extends AbstractSimpleFilter{
 
     @Override
     public boolean filter(VcfLine vcfLine, Settings settings) {
-	return settings.getNotChr().containsKey(vcfLine.getChr());
+        // chr input implementation (gets priority over the bed file)
+        if (settings.getNotChr().containsKey(vcfLine.getChr())) {
+            // loop trough list of positions in chromosome
+            for (Object i:(Collection) settings.getNotChr().get(vcfLine.getChr())) {
+                // if value is null the whole chromosome is accepted
+                if (i == null) { return false; }
+                // change positions collection item to list
+                List<Integer> il = (List) i;
+                // look for specific positions that are passed on
+                if((vcfLine.getPosition() >= (Integer) il.get(0)) && (vcfLine.getPosition() <= (Integer) il.get(1))) { return false; }          
+            }
+        }
+        // bed implementation 
+        if (settings.getExludeBed().containsKey(vcfLine.getChr())) {
+            // loop trough list of positions in chromosome
+            for (Object i:(Collection) settings.getExludeBed().get(vcfLine.getChr())) {
+                // change positions collection item to list
+                List<Integer> il = (List) i;
+                // look for specific positions that are passed on
+                if((vcfLine.getPosition() >= (Integer) il.get(0)) && (vcfLine.getPosition() <= (Integer) il.get(1))) { return false; }          
+            }
+        }
+        return true;
     }
     
 }
