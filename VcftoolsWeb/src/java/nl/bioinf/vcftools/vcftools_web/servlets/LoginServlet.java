826965968aaa634dcd83f1d0bb6be144a71d31a2
jlodewijk <jlodewijk@st.hanze.nl>
2014-01-13 13:11:17 +0100

    Fixed non-static bug and LoginServlet has proper registration

--- VcftoolsWeb/src/java/nl/bioinf/vcftools/vcftools_web/servlets/LoginServlet.java
+++ VcftoolsWeb/src/java/nl/bioinf/vcftools/vcftools_web/servlets/LoginServlet.java
@@ -1,14 +1,24 @@
 package nl.bioinf.vcftools.vcftools_web.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import nl.bioinf.vcftools.vcftools_web.db.DbConnector;
 
 public class LoginServlet extends HttpServlet {
+    private Connection connection;
 
+    /**
+     * Makes contact with the database.
+     */
+
+    public LoginServlet() {
+        connection = DbConnector.getConnection();
+    }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -18,23 +28,23 @@ public class LoginServlet extends HttpServlet {
         String pass = request.getParameter("password");
         // If user presses the button login
         if (userinput.equals("Login")) {
-            if (ValidateUser.checkUserInformation(name, pass)) {
-                RequestDispatcher rs = request.getRequestDispatcher("welcome.html");
+            if (ValidateUser.checkUserInformation(connection,name, pass)) {
+                RequestDispatcher rs = request.getRequestDispatcher("login.jsp");
                 rs.forward(request, response);
             } else {
-                RequestDispatcher rs = request.getRequestDispatcher("login.jsp");
+                RequestDispatcher rs = request.getRequestDispatcher("error.html");
                 rs.forward(request, response);
             }
 
         }// Else user pressed the register button.
         else {
             // To prevent duplicate usernames, since the usernames are the primary key.
-            if (ValidateUser.doesUserNameExist(name)) {
+            if (ValidateUser.doesUserNameExist(connection,name)) {
                 RequestDispatcher rs = request.getRequestDispatcher("error.html");
                 rs.forward(request, response);
             } else {
-                CreateUser.insertNewUser(name, pass);
-                RequestDispatcher rs = request.getRequestDispatcher("welcome.html");
+                CreateUser.insertNewUser(connection,name, pass);
+                RequestDispatcher rs = request.getRequestDispatcher("login.jsp");
                 rs.forward(request, response);
             }
         }
