ad1723fb22fb504e796e30a7431584a9f8725b06
Sergio <Sergio@notebook>
2014-01-18 15:11:34 +0100

    Fixed filterdependencies

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterDependencies.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterDependencies.java
@@ -11,47 +11,71 @@ import nl.bioinf.vcftools.filehandlers.VcfGenotype;
 import nl.bioinf.vcftools.filehandlers.VcfLine;
 
 /**
- *
+ *  Collects data of the individual for filter use
  * @author Sergio Bondietti <sergio@bondietti.nl>
  */
 public class FilterDependencies {
-    private HashMap<Integer, Integer> dp;
-    private HashMap<Integer, Integer> count;
+    private HashMap<Integer, Integer> totalDp;
+    private HashMap<Integer, Double> meanDp;
+    private HashMap<Integer, Boolean> Phased;
+    private int siteCount;
     
     
     /**
      * Default constructor
      */
     public FilterDependencies() {
-        this.dp = new HashMap<>();
-        this.count = new HashMap<>();
+        this.totalDp = new HashMap<>();
+        this.meanDp = new HashMap<>();
+        this.Phased = new HashMap<>();
+        this.siteCount = 0;
     }
-        
-        
+             
     /**
-     * Collect dependencies of vcfLine
+     * Collect dependencies from a VcfLine object
      * @param vcfLine 
      */
     public void collectDependencies(VcfLine vcfLine) {
         int genoNum = vcfLine.getGenotypeNumber();
         for (int i=0;i<genoNum;i++) {
-            VcfGenotype genotype = vcfLine.getGenotype(i);
-            
-            if (this.dp.containsKey(i)) { this.dp.put(i, (this.dp.get(i) + genotype.getDp())); }
-            else { this.dp.put(i, genotype.getDp()); }
-            
-            if (this.count.containsKey(i)) { this.count.put(i, (this.count.get(i) + 1)); }
-            else { this.count.put(i, 1); }
-            //System.out.println(vcfLine.getGenotype(i));
+            VcfGenotype genotype = vcfLine.getGenotype(i);           
+            // If first collection of data then prefil datasets
+            if (this.siteCount == 0) {
+                this.totalDp.put(i, 0);
+                this.Phased.put(i, false);
+            }        
+            // Store depths total
+            this.totalDp.put(i, (this.totalDp.get(i) + genotype.getDp()));
+            // When we find a phased the whole individual is one totally unphased and can be kept
+            if (genotype.isPhased() == true) { this.Phased.put(i, true); }    
         }
+        // The original vcftools also calculates size when genotype is empty so one combined total counter will be okay
+        this.siteCount++;
     }
     
     /**
      * Calculate dependencies after all collecting is finished
      */
     public void calculateDependencies() {
-        //System.out.println(this.dp);
-        //System.out.println(this.count);
+        for (Integer key : this.totalDp.keySet()) {
+            this.meanDp.put(key, ((double)this.totalDp.get(key) / (double)this.siteCount));
+        }
     }
+
+    /**
+     * Get a list of meanDP per individual
+     * @return 
+     */
+    public HashMap<Integer, Double> getMeanDp() {
+        return meanDp;
+    }
+    
+    /**
+     * Get if there is phased data per individual, if its all unphased it returns false
+     * @return 
+     */
+    public HashMap<Integer, Boolean> getPhased() {
+        return Phased;
+    } 
     
 }
