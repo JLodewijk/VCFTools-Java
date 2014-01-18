b2f8d687a9693f88f6ecc06b30d85330d8076e82
Jeroen Lodewijk <j.lodewijk@st.hanze.nl>
2014-01-18 14:09:15 +0100

    Added genotype and bug fix in MeanDepth

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/site/MeanDepth.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/site/MeanDepth.java
@@ -3,22 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package nl.bioinf.vcftools.filters.site;
 
 import nl.bioinf.vcftools.Settings;
+import nl.bioinf.vcftools.filehandlers.VcfGenotype;
 import nl.bioinf.vcftools.filters.AbstractSiteFilter;
 import nl.bioinf.vcftools.filehandlers.VcfLine;
 
 /**
  *
  * @author mhroelfes <marcoroelfes@gmail.com>
+ * @author Jeroen Lodewijk <j.lodewijk@st.hanze.nl>
  */
 public class MeanDepth extends AbstractSiteFilter {
 
     @Override
     public boolean filter(VcfLine vcfLine, Settings settings) {
-        return settings.getMinMeanDp() >= ((double)vcfLine.getDp()/(double)vcfLine.getGenotypeNumber())  && settings.getMaxMeanDp() <= ((double)vcfLine.getDp()/(double)vcfLine.getGenotypeNumber());
+        int genoNum = vcfLine.getGenotypeNumber();
+        double genoDept = 0;
+        for (int i = 0; i < genoNum; i++) {
+            VcfGenotype genotype = vcfLine.getGenotype(i);
+            genoDept = genoDept + genotype.getDp();
+        }
+        return settings.getMinMeanDp() <= (genoDept / (double) vcfLine.getGenotypeNumber()) && settings.getMaxMeanDp() >= (genoDept / (double) vcfLine.getGenotypeNumber());
+
     }
-    
+
 }
