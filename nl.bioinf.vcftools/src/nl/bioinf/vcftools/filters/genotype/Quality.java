cdac82450cd05ec86af18b462865b78e1717a95f
Marco <Marco@Marco-PC.lan>
2014-01-19 01:53:31 +0100

    added genotype filter qualityh(genotype options are not yet implemented in settings and CLI!)

---
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/genotype/Quality.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nl.bioinf.vcftools.filters.genotype;
+
+import java.util.ArrayList;
+import java.util.List;
+import nl.bioinf.vcftools.Settings;
+import nl.bioinf.vcftools.filehandlers.VcfLine;
+import nl.bioinf.vcftools.filters.AbstractGenotypeFilter;
+
+/**
+ *
+ * @author Marco
+ */
+public class Quality extends AbstractGenotypeFilter{
+
+    @Override
+    public List<Boolean> filter(VcfLine vcfLine, Settings settings) {
+        int number = vcfLine.getGenotypeNumber();
+        List<Boolean> keep = new ArrayList<Boolean>();
+        for(int i=1; i<number++; i++){
+           double gq = vcfLine.getGenotype(i).getGq();
+            if(gq>minGQ){
+                keep.add(true);
+            }else{
+                keep.add(false);
+            }
+            
+        }
+        return keep;
+    }
+        
+    
+}
