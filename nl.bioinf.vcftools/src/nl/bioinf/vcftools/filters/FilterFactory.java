fabc058ebebe4ee5f4807f11bb48b48c29a70e10
Sergio <Sergio@notebook>
2014-01-19 13:12:42 +0100

    Updated filter factory and handler to work with genotypes

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterFactory.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterFactory.java
@@ -26,6 +26,8 @@ import nl.bioinf.vcftools.filters.site.RemoveFiltered;
 import nl.bioinf.vcftools.filters.site.NonRefAlleleFrequencies;
 import java.util.ArrayList;
 import nl.bioinf.vcftools.Settings;
+import nl.bioinf.vcftools.filters.genotype.Depth;
+import nl.bioinf.vcftools.filters.genotype.Quality;
 import nl.bioinf.vcftools.filters.site.ExcludeSnp;
 import nl.bioinf.vcftools.filters.site.Mask;
 
@@ -35,54 +37,62 @@ import nl.bioinf.vcftools.filters.site.Mask;
  */
 public class FilterFactory {
     private Settings settings;
-    private ArrayList<AbstractSiteFilter> simpleFilters;
+    private ArrayList<AbstractSiteFilter> siteFilters;
+    private ArrayList<AbstractGenotypeFilter> genotypeFilters;
     
     public FilterFactory(Settings settings) {
         this.settings = settings;
-        this.createSimpleFilters();
+        this.createSiteFilters();
+        this.createGenotypeFilters();
     }
     
-    public void createSimpleFilters() {
-        this.simpleFilters = new ArrayList<>();
-        if ((!this.settings.getChr().isEmpty()) || (!this.settings.getBed().isEmpty())) { this.simpleFilters.add(new IncludeChromosome()); }
-        if ((!this.settings.getNotChr().isEmpty()) || (!this.settings.getExludeBed().isEmpty())) { this.simpleFilters.add(new ExcludeChromosome()); }
-        if (!this.settings.getSnp().isEmpty()) { this.simpleFilters.add(new IncludeSnp()); }
-        if (!this.settings.getExcludeSnp().isEmpty()) { this.simpleFilters.add(new ExcludeSnp()); }
-        if (!this.settings.getPositions().isEmpty()) { this.simpleFilters.add(new IncludePositions()); }
-        if (this.settings.getKeepInfo() != null) { this.simpleFilters.add(new KeepInfo()); }
+    public void createSiteFilters() {
+        this.siteFilters = new ArrayList<>();
+        if ((!this.settings.getChr().isEmpty()) || (!this.settings.getBed().isEmpty())) { this.siteFilters.add(new IncludeChromosome()); }
+        if ((!this.settings.getNotChr().isEmpty()) || (!this.settings.getExludeBed().isEmpty())) { this.siteFilters.add(new ExcludeChromosome()); }
+        if (!this.settings.getSnp().isEmpty()) { this.siteFilters.add(new IncludeSnp()); }
+        if (!this.settings.getExcludeSnp().isEmpty()) { this.siteFilters.add(new ExcludeSnp()); }
+        if (!this.settings.getPositions().isEmpty()) { this.siteFilters.add(new IncludePositions()); }
+        if (this.settings.getKeepInfo() != null) { this.siteFilters.add(new KeepInfo()); }
         if (this.settings.isKeepIndels() != null) {
-            if (this.settings.isKeepIndels() == true) { this.simpleFilters.add(new KeepIndels()); }
-            if (this.settings.isKeepIndels() == false) { this.simpleFilters.add(new RemoveIndels()); }
+            if (this.settings.isKeepIndels() == true) { this.siteFilters.add(new KeepIndels()); }
+            if (this.settings.isKeepIndels() == false) { this.siteFilters.add(new RemoveIndels()); }
         }
         if (this.settings.isRemoveFilteredAll() != null) {
-            if (this.settings.isRemoveFilteredAll() == true) { this.simpleFilters.add(new RemoveFiltered()); }
+            if (this.settings.isRemoveFilteredAll() == true) { this.siteFilters.add(new RemoveFiltered()); }
         }
-        if (!this.settings.getRemoveFiltered().isEmpty()) { this.simpleFilters.add(new RemoveSpecificFilter()); }
-        if (!this.settings.getKeepFiltered().isEmpty()) { this.simpleFilters.add(new KeepSpecificFilter()); }
-        if (!this.settings.getRemoveInfo().isEmpty()) { this.simpleFilters.add(new RemoveInfo()); }
-        if (!this.settings.getKeepInfo().isEmpty()) { this.simpleFilters.add(new KeepInfo()); }
-        if (this.settings.getMinQ() != null) { this.simpleFilters.add(new MinimalQuality()); }
-        if ((this.settings.getMinMeanDp() != null) && (this.settings.getMaxMeanDp() != null)) { this.simpleFilters.add(new MeanDepth()); }
-        if ((this.settings.getMaf() != null) && (this.settings.getMaxMaf() != null)) { this.simpleFilters.add(new AlleleFrequencies()); }
-        if ((this.settings.getNonRefAf() != null) && (this.settings.getMaxNonRefAf() != null)) { this.simpleFilters.add(new NonRefAlleleFrequencies()); }
+        if (!this.settings.getRemoveFiltered().isEmpty()) { this.siteFilters.add(new RemoveSpecificFilter()); }
+        if (!this.settings.getKeepFiltered().isEmpty()) { this.siteFilters.add(new KeepSpecificFilter()); }
+        if (!this.settings.getRemoveInfo().isEmpty()) { this.siteFilters.add(new RemoveInfo()); }
+        if (!this.settings.getKeepInfo().isEmpty()) { this.siteFilters.add(new KeepInfo()); }
+        if (this.settings.getMinQ() != null) { this.siteFilters.add(new MinimalQuality()); }
+        if ((this.settings.getMinMeanDp() != null) && (this.settings.getMaxMeanDp() != null)) { this.siteFilters.add(new MeanDepth()); }
+        if ((this.settings.getMaf() != null) && (this.settings.getMaxMaf() != null)) { this.siteFilters.add(new AlleleFrequencies()); }
+        if ((this.settings.getNonRefAf() != null) && (this.settings.getMaxNonRefAf() != null)) { this.siteFilters.add(new NonRefAlleleFrequencies()); }
         if ((this.settings.getMac() != null) && (this.settings.getMaxMac() != null)) {  }
         if ((this.settings.getNonRefAc() != null) && (this.settings.getMaxNonRefAc() != null)) { /* NonRefAlleleCount() */ }
         if (this.settings.getHwe() != null) { /* Hardy() */ }
         if (this.settings.getGeno() != null) { /* ?? */ }
-        if (this.settings.getMaxMissingCount() != null) { this.simpleFilters.add(new MissingCount()); }
-        if ((this.settings.getMinAlleles() != null) && (this.settings.getMaxAlleles() != null)) { this.simpleFilters.add(new MinorAlleleCount()); }
-        if (this.settings.getThin() != null) { this.simpleFilters.add(new Thinning()); }
-        if (this.settings.getMaskFile() != null) {this.simpleFilters.add(new Mask());}
-// to do add mask
+        if (this.settings.getMaxMissingCount() != null) { this.siteFilters.add(new MissingCount()); }
+        if ((this.settings.getMinAlleles() != null) && (this.settings.getMaxAlleles() != null)) { this.siteFilters.add(new MinorAlleleCount()); }
+        if (this.settings.getThin() != null) { this.siteFilters.add(new Thinning()); }
+        if (this.settings.getMaskFile() != null) {this.siteFilters.add(new Mask());}
+    }
+    
+    public void createGenotypeFilters() {
+        if ((this.settings.getMinDp() != null) && (this.settings.getMaxDp() != null)) { this.genotypeFilters.add(new Depth()); }
+        if (this.settings.getMinGq() != null) { this.genotypeFilters.add(new Quality()); }
     }
 
-    public ArrayList<AbstractSiteFilter> getSimpleFilters() {
-        return simpleFilters;
+    public ArrayList<AbstractSiteFilter> getSiteFilters() {
+        return siteFilters;
     }
 
-    public void setSimpleFilters(ArrayList<AbstractSiteFilter> simpleFilters) {
-        this.simpleFilters = simpleFilters;
+    public ArrayList<AbstractGenotypeFilter> getGenotypeFilters() {
+        return genotypeFilters;
     }
+
+    
     
     
     
--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterHandler.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterHandler.java
@@ -6,6 +6,8 @@
 package nl.bioinf.vcftools.filters;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import nl.bioinf.vcftools.Settings;
 import nl.bioinf.vcftools.filters.FilterFactory;
 import nl.bioinf.vcftools.filehandlers.VcfLine;
@@ -17,11 +19,13 @@ import nl.bioinf.vcftools.filehandlers.VcfLine;
 public class FilterHandler {
     private Settings settings;
     private ArrayList<AbstractSiteFilter> siteFilters;
+    private ArrayList<AbstractGenotypeFilter> genotypeFilters;
 
     public FilterHandler(Settings settings) {
         this.settings = settings;
         FilterFactory filterFactory = new FilterFactory(this.settings);
-        this.siteFilters = filterFactory.getSimpleFilters();
+        this.siteFilters = filterFactory.getSiteFilters();
+        this.genotypeFilters = filterFactory.getGenotypeFilters();
     }
 
     /**
@@ -37,7 +41,23 @@ public class FilterHandler {
             }
         }
        return true;
-
     }
-
+    
+    public List<Boolean> performGenotypeFilters(VcfLine vcfLine) {
+       // Create and prefull hashmap with true values 
+       List<Boolean> result = new ArrayList<>();
+       int genoNum = vcfLine.getGenotypeNumber();
+       for (int i=0;i<genoNum;i++) { result.add(true); }
+       // Perform filters
+       for (AbstractGenotypeFilter i : this.genotypeFilters) {
+           // Filter then Check if there are false values genotype result and change full result into false if so.
+           List<Boolean> filterResult = i.filter(vcfLine, this.settings);
+           int index = 0;
+           for (Boolean fi : filterResult) { 
+               if (fi == false) { result.set(index, false); }
+               index++;
+           }
+        }
+       return result;
+    }
 }
