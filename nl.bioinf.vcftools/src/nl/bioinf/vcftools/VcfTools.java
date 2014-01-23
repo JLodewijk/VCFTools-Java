74829a6c8ad36966adb4dee916da5668a9da707e
Jeroen Lodewijk <j.lodewijk@st.hanze.nl>
2014-01-23 12:20:26 +0100

    Removed useless code in VcfTools

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/VcfTools.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/VcfTools.java
@@ -20,26 +20,11 @@ public class VcfTools {
      * @throws org.apache.commons.cli.ParseException
      */
     public static void main(String[] args) throws ParseException {
-        // TODO code application logic here
-        Settings settingstest = new Settings();
-        
-//        try {
-//            settingstest.save("defaultConfig.xml");
-//        } catch (IOException ex) {
-//            Logger.getLogger(VcfTools.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        -notChr momo -notChr lastNot -fromBp 50 -toBp 100 -chr bleep -fromBp 5 -toBp 9 -chr blub -notChr bobiie
         
         Cli cli = new Cli(args);
         Settings settings = cli.getSettings();
-        //System.out.println("chr = "+settings.getChr());
-        //System.out.println("not chr ="+settings.getNotChr());
-//        System.out.println(settings.getNotChr());
         VcfProcessor vcfReader = new VcfProcessor(settings);
 
-//        System.out.println("bleep");
-//        System.out.println(settings.getInputFile());
-//       System.out.println(Settings.getInstance().getInputFile());
     }
 
 }
