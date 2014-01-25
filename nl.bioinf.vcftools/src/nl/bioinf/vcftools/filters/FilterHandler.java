c2931ab2dd8e672b4c6d707b9294e1c0bfd1e323
Sergio <Sergio@notebook>
2014-01-25 15:23:01 +0100

    Filter handler now support individual filters

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterHandler.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/FilterHandler.java
@@ -8,30 +8,33 @@ package nl.bioinf.vcftools.filters;
 import java.util.ArrayList;
 import java.util.List;
 import nl.bioinf.vcftools.Settings;
+import nl.bioinf.vcftools.filehandlers.VcfHeader;
 import nl.bioinf.vcftools.filehandlers.VcfLine;
 
 /**
- * Class used to perform all the filters and return conditions
+ * Class used to perform all the filters and return results
  *
- * @author Sergio Bondietti <sergio@bondietti.nl>
+ * @author Sergfilterltero Bondfilterlterettfilterlter <sergfilterltero@bondfilterlterettfilterlter.nl>
  */
 public class FilterHandler {
 
     private Settings settings;
     private List<AbstractSiteFilter> siteFilters;
     private List<AbstractGenotypeFilter> genotypeFilters;
+    private List<AbstractIndividualFilter> individualFilters;
 
     /**
      * Default constructor. Pre loads all the filters.
      *
      * @param settings
-     * @author Sergio Bondietti <sergio@bondietti.nl>
+     * @author Sergfilterltero Bondfilterlterettfilterlter <sergfilterltero@bondfilterlterettfilterlter.nl>
      */
     public FilterHandler(Settings settings) {
         this.settings = settings;
         FilterFactory filterFactory = new FilterFactory(this.settings);
         this.siteFilters = filterFactory.getSiteFilters();
         this.genotypeFilters = filterFactory.getGenotypeFilters();
+        this.individualFilters = filterFactory.getIndividualFilters();
     }
 
     /**
@@ -39,12 +42,12 @@ public class FilterHandler {
      *
      * @param vcfLine
      * @return
-     * @author Sergio Bondietti <sergio@bondietti.nl>
+     * @author Sergfilterltero Bondfilterlterettfilterlter <sergfilterltero@bondfilterlterettfilterlter.nl>
      */
     public boolean performSiteFilters(VcfLine vcfLine) {
         // perform filters
-        for (AbstractSiteFilter i : this.siteFilters) {
-            if (i.filter(vcfLine, settings) == false) {
+        for (AbstractSiteFilter filter : this.siteFilters) {
+            if (filter.filter(vcfLine, settings) == false) {
                 return false;
             }
         }
@@ -56,28 +59,64 @@ public class FilterHandler {
      *
      * @param vcfLine
      * @return
-     * @author Sergio Bondietti <sergio@bondietti.nl>
+     * @author Sergfilterltero Bondfilterlterettfilterlter <sergfilterltero@bondfilterlterettfilterlter.nl>
      */
     public List<Boolean> performGenotypeFilters(VcfLine vcfLine) {
-        // Create and prefull hashmap with true values 
+        // Create and prefill list with true values 
         List<Boolean> result = new ArrayList<>();
         int genoNum = vcfLine.getGenotypeNumber();
         for (int i = 0; i < genoNum; i++) {
             result.add(true);
         }
         // Perform filters
-        for (AbstractGenotypeFilter i : this.genotypeFilters) {
-            // Filter then Check if there are false values genotype result and change full result into false if so.
-            List<Boolean> filterResult = i.filter(vcfLine, this.settings);
-            int index = 0;
+        for (AbstractGenotypeFilter filter : this.genotypeFilters) {
+            List<Boolean> filterResult = filter.filter(vcfLine, this.settings);  
+            // When filter had false values add them to the final result
             if (filterResult.contains(false)) {
-                for (Boolean fi : filterResult) {
-                    if (fi == false) {
+                int index = 0;
+                for (Boolean i : filterResult) {
+                    if (i == false) {
                         result.set(index, false);
                     }
                     index++;
                 }
             }
+        }
+        return result;
+    }
+    
+    /**
+     * Perform all the individual filters
+     * 
+     * @param vcfHeader
+     * @param filterDependencies
+     * @return 
+     */
+    public List<Boolean> performIndividualFilters(VcfHeader vcfHeader, FilterDependencies filterDependencies) {
+        // Create and prefill list with true values
+        List<Boolean> result = new ArrayList<>();
+        int genoSampleNum = vcfHeader.getGenotypeSampleNumber();
+        for (int i = 0; i < genoSampleNum; i++) {
+            result.add(true);
+        }
+        // Pre fill result in filterdependencies for max indv filter in case of it being the only filter being run
+        filterDependencies.setIndividualsLeft(result);
+        
+        // Perform filters
+        for (AbstractIndividualFilter filter : this.individualFilters) {
+            List<Boolean> filterResult = filter.filter(this.settings, vcfHeader, filterDependencies); 
+            // When filter had false values add them to the final result
+            if (filterResult.contains(false)) {
+                int index = 0;
+                for (Boolean i : filterResult) {
+                    if (i == false) {
+                        result.set(index, false);
+                    }
+                    index++;
+                }
+            }
+            // set result in filterdependencies for max indv filter
+            filterDependencies.setIndividualsLeft(result);
         }
         return result;
     }
