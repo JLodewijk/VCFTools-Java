6275921f4b626458a784e100a74e9fd886de32d0
Marco <Marco@Marco-PC.lan>
2014-01-28 14:42:35 +0100

    added javadoc

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/site/Thinning.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/filters/site/Thinning.java
@@ -16,7 +16,13 @@ import nl.bioinf.vcftools.filehandlers.VcfLine;
 public class Thinning extends AbstractSiteFilter {
 
     int positionPreviousSnp;
-
+     /**
+     * filters a vcf line based on position of the SNP, if a SNP is too close to another it returns false and the line is rejected.
+     * @param vcfLine
+     * @param settings
+     * @return
+     * @author mhroelfes <marcoroelfes@gmail.com>
+     */
     @Override
     public boolean filter(VcfLine vcfLine, Settings settings) {
         //check if minsnpDist is valid
