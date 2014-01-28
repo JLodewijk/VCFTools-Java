b5eeb382cbff7a38738afece86dd99a11d4545e9
Sergio <Sergio@notebook>
2014-01-28 13:34:07 +0100

    Removed unused import

--- nl.bioinf.vcftools/src/nl/bioinf/vcftools/Settings.java
+++ nl.bioinf.vcftools/src/nl/bioinf/vcftools/Settings.java
@@ -8,7 +8,6 @@ package nl.bioinf.vcftools;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
